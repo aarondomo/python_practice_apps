5fefa8325945df1fefd226f6c8a563b61f809c09
yaxhiro <39930133+yaxhiro@users.noreply.github.com>
2022-06-06 20:26:05 -0500

    Country Field added (#5)
    * Country Field added
    
    * Ahi esta tu P* espacio

--- GetRandomUsersApp/main.py
+++ GetRandomUsersApp/main.py
@@ -7,21 +7,12 @@ root.geometry("500x500")
 
 user_response = get_response("https://randomuser.me/api/?results=2")
 user_obj = parse_user(user_response.content)
-text_entry = tk.Entry(root)
-text_entry.pack()
-retrieve_button = tk.Button(root, text="Get Users", command="get_users()")
-retrieve_button.pack()
-
 
 for user in user_obj.results:
 	first_name_label = tk.Label(root, text="First name: " + user.name.first)
 	first_name_label.pack()
 	last_name_label = tk.Label(root, text="Last name: " + user.name.last)
 	last_name_label.pack()
-	country_field_label = tk.Label(root, text="Country: " + user.location.country)
-	country_field_label.pack()
-
-
 
 json_label = tk.Label(root, text=user_response.content, wraplength=500, justify="center")
 json_label.pack()
